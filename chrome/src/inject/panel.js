97a4b7326b566881a0f04f2b7ee746aa97927877
Patrick Thach <thachp@gmail.com>
2013-12-07 11:51:09 -0800

    Started working on logo.

--- chrome/src/inject/panel.js
+++ chrome/src/inject/panel.js
@@ -2,32 +2,19 @@
  * @author      Patrick Thach
  */
 
-var PANEL = function () {
-
-    return {
-
-        init:function () {
-            this.click();
-            $(".tab-pane").hide();
-        },
-
-        click: function()
-        {
-            $(".tab").click(function(el)
-            {
-                el.preventDefault();
-                $(".tab-pane").hide();
-
-                $($(this).attr("href")).show("slow");
-
-                alert("got here!");
-
-            });
-        }
-
-    }; //end return
-}(); // end
-
 $(function () {
-    PANEL.init();
-}); //scripts loaded ready to rock and roll
+    $(".tab-pane").hide();
+    $("#contribute").show();
+
+    $(".tab").click(function(el)
+    {
+        el.preventDefault();
+
+        $(".nav li").removeClass("active");
+
+        $(".tab-pane").hide();
+        $(this).parent().attr("class","active").css("color","white");
+        $($(this).attr("href")).show("slow");
+    });
+
+});
