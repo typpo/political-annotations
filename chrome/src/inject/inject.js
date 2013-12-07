a845808386bc4703de326d932893906f3fb8d86e
Ian Webster <typppo@gmail.com>
2013-12-07 01:00:13 -0800

    working legislature endpoint serving the politicians

--- chrome/src/inject/inject.js
+++ chrome/src/inject/inject.js
@@ -55,40 +55,74 @@ var MOUSEOVER_TIMEOUT_MS = 700;
     }, MOUSEOVER_TIMEOUT_MS);
   });
 
+  loadSenators($, function() {
+    chrome.storage.local.get('Ted Cruz', function(data) {
+      if(chrome.runtime.lastError) {
+        console.error(chrome.runtime.lastError);
+        return;
+      }
+      console.log(data);
+    });
+  });
   console.log('Done.');
 })(jQuery);
 
+function loadSenators($, callback) {
+  console.log('Checking senators');
+  chrome.storage.local.get('synced', function(data) {
+    if(chrome.runtime.lastError) {
+      console.error(chrome.runtime.lastError);
+      return;
+    }
+    if (data) {
+      console.log('already have');
+      callback();
+      return;
+    }
+    console.log('Loading senators');
+
+    $.getJSON('http://localhost:5000/legislature', function(data) {
+      var legislators = data.results;
+      for (var i=0; i < legislators.length; i++) {
+        var legislator = legislators[i];
+        obj = {};
+        obj[legislator.first_name + ' ' + legislator.last_name] = legislator;
+        chrome.storage.local.set(obj);
+      }
+      console.log(data);
+      callback();
+    });
+  });
+}
+
 // John Resig - http://ejohn.org/ - MIT Licensed
-(function(){
-  var cache = {};
+var cache = {};
+function tmpl(str, data) {
+  // Figure out if we're getting a template, or if we need to
+  // load the template - and be sure to cache the result.
+  var fn = !/\W/.test(str) ?
+    cache[str] = cache[str] ||
+      tmpl(document.getElementById(str).innerHTML) :
 
-  this.tmpl = function tmpl(str, data){
-    // Figure out if we're getting a template, or if we need to
-    // load the template - and be sure to cache the result.
-    var fn = !/\W/.test(str) ?
-      cache[str] = cache[str] ||
-        tmpl(document.getElementById(str).innerHTML) :
+    // Generate a reusable function that will serve as a template
+    // generator (and which will be cached).
+    new Function("obj",
+      "var p=[],print=function(){p.push.apply(p,arguments);};" +
 
-      // Generate a reusable function that will serve as a template
-      // generator (and which will be cached).
-      new Function("obj",
-        "var p=[],print=function(){p.push.apply(p,arguments);};" +
+      // Introduce the data as local variables using with(){}
+      "with(obj){p.push('" +
 
-        // Introduce the data as local variables using with(){}
-        "with(obj){p.push('" +
+      // Convert the template into pure JavaScript
+      str
+        .replace(/[\r\t\n]/g, " ")
+        .split("<%").join("\t")
+        .replace(/((^|%>)[^\t]*)'/g, "$1\r")
+        .replace(/\t=(.*?)%>/g, "',$1,'")
+        .split("\t").join("');")
+        .split("%>").join("p.push('")
+        .split("\r").join("\\'")
+    + "');}return p.join('');");
 
-        // Convert the template into pure JavaScript
-        str
-          .replace(/[\r\t\n]/g, " ")
-          .split("<%").join("\t")
-          .replace(/((^|%>)[^\t]*)'/g, "$1\r")
-          .replace(/\t=(.*?)%>/g, "',$1,'")
-          .split("\t").join("');")
-          .split("%>").join("p.push('")
-          .split("\r").join("\\'")
-      + "');}return p.join('');");
-
-    // Provide some basic currying to the user
-    return data ? fn( data ) : fn;
-  };
-})();
+  // Provide some basic currying to the user
+  return data ? fn( data ) : fn;
+}
