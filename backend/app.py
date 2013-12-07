716c2a3ba2814185c165074335344431d4d2ffad
Alexander <alexandermichaelmiranda@gmail.com>
2013-12-07 01:42:55 -0800

    endpoint for legislators

--- backend/app.py
+++ backend/app.py
@@ -12,7 +12,18 @@ def index():
 def people():
   r = requests.get('http://congress.api.sunlightfoundation.com/legislators?chamber=senate&apikey=dde4e99ca38e411abbc7d13af84ecbc0')
   obj = json.loads(r.text)
-  return jsonify(obj)
+  returnObject = list()
+  i = 0
+  while(i < len(obj)):
+    fname = obj['results'][i]['first_name']
+    lname = obj['results'][i]['last_name']
+    mname = obj['results'][i]['middle_name']
+    bioguide_id = obj['results'][i]['bioguide_id']
+    temp = {'first_name':fname, 'last_name': lname, 'middle_name': mname, 'bioguide_id': bioguide_id}
+    returnObject.append(temp)
+    i += 1 
+  results = {'results':returnObject}  
+  return jsonify(results)
 
   #JSON decode into list = total list of senators
   #temp JSON or list = return
