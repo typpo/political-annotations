3631d8c6538c25ef5c5b3efd1e1d7940fdc85595
Ian <typppo@gmail.com>
2013-12-07 00:04:24 -0800

    people endpoint

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,27 @@
 #!/usr/bin/env python
-from flask import Flask
+import requests
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route("/")
 def index():
   return "Hello World!"
 
+@app.route("/people")
+def people():
+  r = requests.get('http://congress.api.sunlightfoundation.com/legislators?chamber=senate&apikey=dde4e99ca38e411abbc7d13af84ecbc0')
+  return jsonify(r.text)
+
+  #JSON decode into list = total list of senators
+  #temp JSON or list = return
+  #while(len(list) != 0)
+    #fname = first name index at pos
+    #lname = last name index at pos
+    #mname = middle name index at pos
+    #bioguide_id = index at pos
+    #create a new list or new JSON of first name, last name, mname, id
+    #temp push to return array or JSON
+  #return or echo return
+
 if __name__ == "__main__":
-  app.run()
+  app.run(debug=True)
