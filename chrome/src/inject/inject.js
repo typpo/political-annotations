30fcd6073ab934b26a5b9fc00a7aee775e0e1778
Ian <typppo@gmail.com>
2013-12-06 22:48:49 -0800

    highlight john's name

--- chrome/src/inject/inject.js
+++ chrome/src/inject/inject.js
@@ -1,3 +1,7 @@
+// Test me:
+// http://www.factcheck.org/2013/12/boehner-vs-castro-on-the-exchange/
+//
+
 console.log('Injected.');
 
 (function() {
@@ -7,8 +11,13 @@ console.log('Injected.');
   // Get politicians
 
   // Wrap name with span with a special class
+  document.body.innerHTML = document.body.innerHTML.replace(/John Boehner/g, '<span class="_cc_highlight">John Boehner</span>');
 
   // Build/request on hover
+  $('._cc_highlight').on('mouseover', function() {
+    alert('You got me');
+
+  });
 
 
   console.log('Done.');
